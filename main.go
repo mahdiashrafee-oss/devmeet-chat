f949f687d0261a4a07437d3e9b0463c7c3ebd14f
Mahdi Ashrafee <ashrafeem8@gmail.com>
2025-06-08 22:02:40 -0400

    feat: impliment basic login and redirection to home

--- main.go
+++ main.go
@@ -4,21 +4,35 @@ import "fmt"
 import "net/http"
 import "html/template"
 
-func homepage(w http.ResponseWriter, r *http.Request){
+var username string
+var password string
+func login(w http.ResponseWriter, r *http.Request){
 	if r.Method == http.MethodPost{
 		r.ParseForm()
-		username :=r.FormValue("username")
-		password :=r.FormValue("password")
+		username =r.FormValue("username")
+		password =r.FormValue("password")
 		fmt.Println(username)
 		fmt.Println(password)
-	tmpl, _ := template.ParseFiles("templates/login.html")
+		if username == "test" && password == "1234"{
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+
+		tmpl, _ := template.ParseFiles("templates/login.html")
 
 	tmpl.Execute(w, nil)
 
 	}
 } 
+func homepage(w http.ResponseWriter, r *http.Request){
+	tmpl, _ :=  template.ParseFiles("templates/index.html")
+
+	tmpl.Execute(w, username)
+
+}
 func main(){
 	mux := http.NewServeMux()
+	mux.Handle("/login",http.HandlerFunc(login))
 	mux.Handle("/",http.HandlerFunc(homepage))
 
 	server := http.Server{
