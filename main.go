132a3b32caef13c5a79369ee73534c5b762278fb
Mahdi Ashrafee <ashrafeem8@gmail.com>
2025-06-08 20:10:17 -0400

    feat: add login form and print submitted data

--- main.go
+++ main.go
@@ -1,19 +1,25 @@
 package main
 
+import "fmt"
 import "net/http"
 import "html/template"
 
 func homepage(w http.ResponseWriter, r *http.Request){
+	if r.Method == http.MethodPost{
+		r.ParseForm()
+		username :=r.FormValue("username")
+		password :=r.FormValue("password")
+		fmt.Println(username)
+		fmt.Println(password)
 	tmpl, _ := template.ParseFiles("templates/login.html")
 
 	tmpl.Execute(w, nil)
 
-	
-
+	}
 } 
 func main(){
 	mux := http.NewServeMux()
-	mux.Handle("/", http.HandlerFunc(homepage))
+	mux.Handle("/",http.HandlerFunc(homepage))
 
 	server := http.Server{
 			Addr: ":8080",
